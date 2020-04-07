d385ebc45e1f14979fd60a735c19d45486a74efe
fiornrrn <soon20.1>
2020-04-07 12:04:00 +0700

    start DnD creation by refs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,10 +13,18 @@ class App extends React.Component {
         cupboardHeight: 230,
         cupboardWidth: 150,
         doorsQuantity: 2,
-      } 
+      },
+      DnDValues: {
+
+      }
     }
 
     this.onSidebarValuesChange = this.onSidebarValuesChange.bind(this);
+
+    //=============== DnD ================
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
   }
 
   onSidebarValuesChange(e, paramName){
@@ -25,10 +33,28 @@ class App extends React.Component {
     this.setState({sidebarValues: bufArr});
   }
 
+  //==================== DnD =====================
+  onMouseDown(e){
+    console.log(e);
+    this.setState({isDown: true});
+  }
+  
+  onMouseUp(e){
+    console.log(e);
+    this.setState({isDown: false});
+  }
+  onMouseMove(e){
+    if(this.state.isDown == true){
+      this.setState({X: e.clientX - 90});
+    }
+  }
+
+
   render(){
     return (
       <div>
-        <Sidebar sidebarValues={this.state.sidebarValues} onSidebarValuesChange={this.onSidebarValuesChange}/>
+        <Sidebar sidebarValues={this.state.sidebarValues} onSidebarValuesChange={this.onSidebarValuesChange}
+          DnDValues={this.state.DnDValues} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} onMouseMove={this.onMouseMove} />
         <WorkingSpace sidebarValues={this.state.sidebarValues}/>
       </div>
     );
--- frontend/src/files/Sidebar.js
+++ frontend/src/files/Sidebar.js
@@ -5,6 +5,7 @@ class Sidebar extends React.Component {
 	constructor(props){
 		super(props);
 	}
+
 	render(){
 		return(
 			<div className='sidebar'>
@@ -12,6 +13,9 @@ class Sidebar extends React.Component {
 				<input type="number" className="number input" value={this.props.sidebarValues.cupboardWidth} onChange={(e) => this.props.onSidebarValuesChange(e, 'cupboardWidth')}/> <i className="text">Ширина</i>
 				<input type="number" className="number input" value={this.props.sidebarValues.doorsQuantity} onChange={(e) => this.props.onSidebarValuesChange(e, 'doorsQuantity')}/> <i className="text">Двери</i>
 				<button className="create-btn">Создать шкаф</button>
+				<img src="/images/profil/light_vertical.png" width={15} height={90} style={{ marginTop: 10, marginLeft: 40}}/>
+				<img src="/images/profil/light_horizontal.png" width={90} height={15} style={{position:"fixed", marginTop: 50, marginLeft: 30}}
+					onMouseDown={this.props.onMouseDown} onMouseUp={this.props.onMouseUp} onMouseMove={this.props.onMouseMove}/>
 			</div>
 		);
 	}
