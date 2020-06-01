6f1020ffdac39103d56b3c976ae2fd80cd49169e
Fiornrrn <fiornrrn@gmail.com>
2020-06-01 20:59:54 +0700

    started reacaptcha creation

--- frontend/src/js/pages/RegisterPage.js
+++ frontend/src/js/pages/RegisterPage.js
@@ -12,6 +12,7 @@ const RegisterPage = (props) => {
          <TextInput styleArr={{marginLeft: "20px", marginTop: "20px", text: "Почта", width: 440}}/>
          <TextInput styleArr={{marginLeft: "20px", marginTop: "100px", text: "Пароль", width: 200}}/>
          <TextInput styleArr={{marginLeft: "260px", marginTop: "100px", text: "Повторите пароль", width: 200}}/>
+         <img src={"./assets/img/GX.svg"} className="GX"/>
       </div>
    </div>
   );
